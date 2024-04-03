bb523593dc000a852242e0f42bc184a9f872d0d0
tflinch <tflinch16@gmail.com>
2024-04-02 22:24:54 -0400

    feat: created header, navbar elements through JS

--- js/app.js
+++ js/app.js
@@ -6,3 +6,32 @@
 
 /*==================== scroll reveal ====================*/
 /*==================== typed js ====================*/
+const navArr = ['home', 'about', 'services', 'portfolio', 'contact'];
+
+const header = document.createElement('header');
+const navbar = document.createElement('nav');
+const logo = document.createElement('a');
+const menuIcon = document.createElement('i');
+
+header.setAttribute('class', 'header');
+header.append(logo, menuIcon, navbar);
+logo.textContent = 'Portfolio';
+logo.setAttribute('class', 'logo');
+menuIcon.setAttribute('class', 'bx bx-menu');
+menuIcon.setAttribute('id', 'menu-icon');
+navbar.setAttribute('class', 'navbar');
+
+navArr.forEach((element) => {
+  const menuItem = document.createElement('a');
+
+  if (element === 'home') {
+    menuItem.setAttribute('class', 'active');
+  }
+
+  menuItem.setAttribute('href', element);
+  menuItem.textContent = element.slice(0, 1).toUpperCase() + element.slice(1);
+
+  navbar.appendChild(menuItem);
+});
+
+document.querySelector('body').appendChild(header);
