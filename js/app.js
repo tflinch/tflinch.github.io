0acea77b19268bde62d7d81e8cd7f5b4798f2b70
tflinch <tflinch16@gmail.com>
2024-04-04 01:43:30 -0400

    feat: style home section for 768px media screen

--- js/app.js
+++ js/app.js
@@ -12,6 +12,8 @@ menuIcon.onclick = () => {
 let header = document.querySelector('header');
 header.classList.toggle('sticy', window.scrollY > 100);
 /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
 
 /*==================== scroll reveal ====================*/
 /*==================== typed js ====================*/
