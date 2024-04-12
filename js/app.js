c36e0ade1a13dbcd117a7ff21e1dd9978ecc74c4
tflinch <tflinch16@gmail.com>
2024-04-11 21:22:42 -0400

    updated sections home contact projects

--- js/app.js
+++ js/app.js
@@ -3,35 +3,46 @@ let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
-  menuIcon.classList.toggle('bx-x');
-  navbar.classList.toggle('active');
-};
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
 /*==================== scroll sections active link ====================*/
 
 /*==================== sticky navbar ====================*/
 let header = document.querySelector('header');
-header.classList.toggle('sticy', window.scrollY > 100);
+header.classList.toggle('sticky', window.scrollY > 100);
+
 /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
 menuIcon.classList.remove('bx-x');
 navbar.classList.remove('active');
-
 /*==================== scroll reveal ====================*/
-ScrollReveal({ reset: true, distance: '880px', duration: 2000, delay: 200 });
-ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-container', '.contact form', {
-  origin: 'bottom',
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 1500,
+    delay: 100
 });
-ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
-ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
-/*==================== typed js ====================*/
 
+ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', {origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img', {origin: 'left'});
+ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right'});
+
+/*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-  strings: ['Frontend Engineer', 'Backend Engineer', 'FullStack Engineer'],
-  typeSpeed: 100,
-  backSpeed: 100,
-  backDelay: 1000,
-  loop: true,
-});
+    strings: ['Frontend Engineer', 'Backend Engineer', 'Fullstack Engineer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+const typedSecond = new Typed('.multiple-text2', {
+    strings: ['user', 'data', 'api', 'iot'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
 
 // const navArr = ['home', 'about', 'services', 'portfolio', 'contact'];
 
